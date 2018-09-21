6cb6a287be24fd4c94b821257044df763956964f
nzh1992 <nzh199266@163.com>
2018-09-22 01:30:34 +0800

    添加装饰器，用来测试函数的执行时间

--- lib/helper.py
+++ lib/helper.py
@@ -3,3 +3,11 @@ created by nzh
 Date: 2018/9/21 上午11:01
 """
 
+from datetime import datetime
+
+# 检测函数运行时间
+def consume_time(func):
+    starttime = datetime.now()
+    func()
+    endtime = datetime.now()
+    print("{0}函数执行了{1}秒", str(func), str(starttime-endtime))
